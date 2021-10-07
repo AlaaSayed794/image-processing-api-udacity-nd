b8240d90e6c065fe7f9d98246e624fb15c4e5903
AlaaSayed794 <alaa.elsayed.aly@gmail.com>
2021-10-08 00:30:39 +0200

    modify server to show image with query

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from "express"
-import logger from "../middlewares/logger"
+import logger from "./middlewares/logger"
+import path from 'path'
+import fs from "fs"
+import { getImagesDir } from "./utils/fsUtils"
+
 const app = express()
 const port = 3000
 app.use([logger])
 
-app.get('/', (req, res) => {
-    res.send("hello")
+app.get('/', async (req, res) => {
+    const filename = req.query.filename
+    if (filename) {
+        const imagesDir = await getImagesDir(__dirname)
+        const imagePath = `${imagesDir}/${filename}.jpg`
+        if (fs.existsSync(imagePath)) {
+            res.sendFile(imagePath)
+        }
+        else {
+            res.status(400)
+            res.send("file name is invalid")
+        }
+    }
+    else {
+        res.status(400)
+        res.send("please provide valid filename as a query parameter")
+    }
 })
 app.listen(port, () => {
     console.log("server started on port: " + port)
---
+++ src/utils/fsUtils.ts
@@ -0,0 +1,15 @@
+import fs from "fs"
+import path from "path"
+
+export const getImagesDir = async (dir: string): Promise<string> => {
+    let imagesDir: string = ""
+    const dirContents: string[] = await fs.readdirSync(dir)
+    if (dirContents.includes("images")) {
+        imagesDir = path.join(dir, "images/full")
+        return imagesDir
+    }
+    else {
+        imagesDir = await getImagesDir(path.join(dir, ".."))
+        return imagesDir
+    }
+}
